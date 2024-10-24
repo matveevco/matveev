3ec71a6b573c4e64b762125b4fbb6620b8755cee
Aleksei Matveev <support@wisart.app>
2024-10-24 16:34:20 +0100

    minor update: 12.0.1 / Fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavigationProvider } from "./components/automation/hooks/NavigationContext";
 import useRouteState from "./components/automation/hooks/useRouteState";
@@ -37,6 +38,7 @@ const General = () => {
         <Route path="*" element={<NoMatch />} />
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 };
